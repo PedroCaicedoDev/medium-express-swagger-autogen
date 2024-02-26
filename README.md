a553e7a82d06af481f3af40e4f15338710d993ba
Pedro Caicedo <caicedo1089@gmail.com>
2024-02-26 18:50:02 -0500

    Added cover image

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # medium-express-swagger-autogen
 
+![Swagger Autogen Express Open API](cover.png)
 Documenta tus APIs fácilamente con open api 3.0 usando swagger autogen
--- src/docs/swagger/autogen.js
+++ src/docs/swagger/autogen.js
@@ -1,11 +1,9 @@
 const autogen = require('swagger-autogen')({openapi: '3.0.0'});
 const configFile = require('./base-config.json');
-const path = require('path');
 
 const load = () => {
     const outputFileName = '../docs/swagger.json';
     const arrEndPointsFiles = [
-        //path.join(__dirname, '../../routes/index.js'),
         '../src/routes/*.js',
     ];
     console.log("arrEndPointsFiles:", arrEndPointsFiles);
